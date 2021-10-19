3fa45fffda193cd1d350919195eeae1f77a2e46b
Carla <carladmaximo@gmail.com>
2021-10-19 19:38:04 -0300

    Requisito 05

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,49 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      artistaPesquisado: '',
+      disabled: true,
+    };
+
+    this.lidaComInput = this.lidaComInput.bind(this);
+  }
+
+  lidaComInput(event) {
+    const { value } = event.target;
+    const NUM_MAX = 2;
+    if (value.length >= NUM_MAX) {
+      this.setState({ disabled: false });
+    }
+    this.setState({
+      artistaPesquisado: value,
+    });
+  }
+
   render() {
+    const { artistaPesquisado, disabled } = this.state;
     return (
       <>
         <Header />
-        <div data-testid="page-search"> Minha página de pesquisa </div>
+        <div data-testid="page-search">
+          <label htmlFor="pesquisaArtista">
+            <input
+              id="pesquisaArtista"
+              data-testid="search-artist-input"
+              value={ artistaPesquisado }
+              onChange={ this.lidaComInput }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ disabled }
+            >
+              Pesquisar
+            </button>
+          </label>
+        </div>
       </>
     );
   }
