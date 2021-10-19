f5fc534da2361281005e717b2255d292fe121c4b
Carla <carladmaximo@gmail.com>
2021-10-19 16:12:40 -0300

    Requisito 01

---
+++ src/pages/ProfileEdit.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class ProfileEdit extends Component {
+  render() {
+    return (<div data-testid="page-profile-edit">Minha página para editar Pefil</div>);
+  }
+}
